16dd6db2ae2edcf3976b1661bb5e78e71394d792
Thibaud <94205515+Gorfort@users.noreply.github.com>
2024-03-27 11:22:11 +0100

    Update server.js

--- nodejswebshop/app/server.js
+++ nodejswebshop/app/server.js
@@ -1,13 +1,9 @@
 const express = require("express");
-
-
 const app = express();
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
-
-
+const userRoute = require("./routes/User");
+app.use("/user", userRoute);
 
 // Démarrage du serveur
 app.listen(8080, () => {
-    console.log('Server running on port 8080');
+  console.log("Server running on port 8080");
 });
